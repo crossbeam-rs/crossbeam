4add437baa82a5f7a304e3b9554f31cac97bd8c9
Steve Klabnik <steve@steveklabnik.com>
2015-09-28 13:11:42 -0400

    Add documentation for scoped threads

--- src/scoped.rs
+++ src/scoped.rs
@@ -40,6 +40,22 @@ pub struct ScopedJoinHandle<T> {
     thread: thread::Thread,
 }
 
+/// Create a new `scope`, for scoped concurrency operations.
+///
+/// Using crossbeam for concurrency involves creating a [`Scope`][scope]. That's the job of
+/// `scope`.
+///
+/// [scope]: struct.Scope.html
+///
+/// # Examples
+///
+/// Creating a scope:
+///
+/// ```
+/// crossbeam::scope(|scope| {
+///     // do stuff with scope here
+/// });
+/// ```
 pub fn scope<'a, F, R>(f: F) -> R where F: FnOnce(&Scope<'a>) -> R {
     let mut scope = Scope { dtors: RefCell::new(None) };
     let ret = f(&scope);
@@ -52,7 +68,7 @@ impl<'a> Scope<'a> {
     // that it can be called directly and, if any dtor panics, can be
     // resumed in the unwinding this causes. By initially running the
     // method outside of any destructor, we avoid any leakage problems
-    // due to #14875.
+    // due to @rust-lang/rust#14875.
     fn drop_all(&mut self) {
         loop {
             // use a separate scope to ensure that the RefCell borrow
@@ -78,6 +94,117 @@ impl<'a> Scope<'a> {
         });
     }
 
+    /// Create a scoped thread.
+    ///
+    /// `spawn` is similar to the [`spawn`][spawn] function in Rust's standard library. The
+    /// difference is that this thread is scoped, meaning that it's guaranteed to terminate
+    /// before the current stack frame goes away, allowing you to reference the parent stack frame
+    /// directly. This is ensured by having the parent thread join on the child thread before the
+    /// scope exits.
+    ///
+    /// [spawn]: http://doc.rust-lang.org/std/thread/fn.spawn.html
+    ///
+    /// # Examples
+    ///
+    /// A basic scoped thread:
+    ///
+    /// ```
+    /// crossbeam::scope(|scope| {
+    ///     scope.spawn(|| {
+    ///         println!("Hello from a scoped thread!");
+    ///     });
+    /// });
+    /// ```
+    ///
+    /// When writing concurrent Rust programs, you'll sometimes see a pattern like this, using
+    /// [`std::thread::spawn`][spawn]:
+    ///
+    /// ```ignore
+    /// let array = [1, 2, 3];
+    /// let mut guards = vec![];
+    ///
+    /// for i in &array {
+    ///     let guard = std::thread::spawn(move || {
+    ///         println!("element: {}", i);
+    ///     });
+    ///
+    ///     guards.push(guard);
+    /// }
+    ///
+    /// for guard in guards {
+    ///     guard.join().unwrap();
+    /// }
+    /// ```
+    ///
+    /// The basic pattern is:
+    ///
+    /// 1. Iterate over some collection.
+    /// 2. Spin up a thread to operate on each part of the collection.
+    /// 3. Join all the threads.
+    ///
+    /// However, this code actually gives an error:
+    ///
+    /// ```text
+    /// error: `array` does not live long enough
+    /// for i in &array {
+    ///           ^~~~~
+    /// in expansion of for loop expansion
+    /// note: expansion site
+    /// note: reference must be valid for the static lifetime...
+    /// note: ...but borrowed value is only valid for the block suffix following statement 0 at ...
+    ///     let array = [1, 2, 3];
+    ///     let mut guards = vec![];
+    ///     
+    ///     for i in &array {
+    ///         let guard = std::thread::spawn(move || {
+    ///             println!("element: {}", i);
+    /// ...
+    /// error: aborting due to previous error
+    /// ```
+    ///
+    /// Because [`std::thread::spawn`][spawn] doesn't know about this scope, it requires a
+    /// `'static` lifetime. One way of giving it a proper lifetime is to [`Box`][box] it up:
+    ///
+    /// [box]: http://doc.rust-lang.org/std/boxed/struct.Box.html
+    ///
+    /// ```
+    /// let array = Box::new([1, 2, 3]);
+    /// let mut guards = vec![];
+    ///
+    /// for i in (0..array.len()) {
+    ///     let i = array[i];
+    ///     let guard = std::thread::spawn(move || {
+    ///         println!("element: {}", i);
+    ///     });
+    ///
+    ///     guards.push(guard);
+    /// }
+    ///
+    /// for guard in guards {
+    ///     guard.join().unwrap();
+    /// }
+    /// ```
+    ///
+    /// But this introduces unnecessary allocation: we know that we're joining the threads before
+    /// our function returns, so just taking a reference _should_ be safe. Rust can't know that,
+    /// though.
+    ///
+    /// Enter scoped threads. Here's our original example, using `spawn` from crossbeam rather
+    /// than from `std::thread`:
+    ///
+    /// ```
+    /// let array = [1, 2, 3];
+    ///
+    /// crossbeam::scope(|scope| {
+    ///     for i in &array {
+    ///         scope.spawn(move || {
+    ///             println!("element: {}", i);
+    ///         });
+    ///     }
+    /// });
+    /// ```
+    ///
+    /// Much more straightforward.
     pub fn spawn<F, T>(&self, f: F) -> ScopedJoinHandle<T> where
         F: FnOnce() -> T + Send + 'a, T: Send + 'a
     {
