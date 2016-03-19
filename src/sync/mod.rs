0992c3719566e824e38fefa264fd6b7bf60f0fca
Steven Fackler <sfackler@gmail.com>
2016-03-19 13:54:58 -0700

    Add an AtomicReference type

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -4,9 +4,11 @@ pub use self::ms_queue::MsQueue;
 pub use self::atomic_option::AtomicOption;
 pub use self::treiber_stack::TreiberStack;
 pub use self::seg_queue::SegQueue;
+pub use self::atomic_reference::AtomicReference;
 
 mod atomic_option;
 mod ms_queue;
 mod treiber_stack;
 mod seg_queue;
 pub mod chase_lev;
+mod atomic_reference;
