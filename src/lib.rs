3521fd94e5f25a6f674aa06d6b864cbf17a29707
Andrew Cann <shum@canndrew.org>
2016-04-18 19:23:38 +0800

    Add scoped thread builder

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,7 @@
             feature(const_fn, repr_simd, optin_builtin_traits))]
 
 use std::thread;
+use std::io;
 
 pub use scoped::{scope, Scope, ScopedJoinHandle};
 
@@ -49,9 +50,19 @@ impl<F: FnOnce()> FnBox for F {
 
 /// Like `std::thread::spawn`, but without the closure bounds.
 pub unsafe fn spawn_unsafe<'a, F>(f: F) -> thread::JoinHandle<()> where F: FnOnce() + Send + 'a {
+    let builder = thread::Builder::new();
+    builder_spawn_unsafe(builder, f).unwrap()
+}
+
+/// Like `std::thread::Builder::spawn`, but without the closure bounds.
+pub unsafe fn builder_spawn_unsafe<'a, F>(builder: thread::Builder, f: F)
+        -> io::Result<thread::JoinHandle<()>>
+    where F: FnOnce() + Send + 'a
+{
     use std::mem;
 
     let closure: Box<FnBox + 'a> = Box::new(f);
     let closure: Box<FnBox + Send> = mem::transmute(closure);
-    thread::spawn(move || closure.call_box())
+    builder.spawn(move || closure.call_box())
 }
+
