32c52907199d2028a6d16180f7ba610b6fb06840
Popog <N/A>
2016-04-19 18:40:39 -0700

    Preparation for new Payload variant
    Removed if tail.is_data()  in preparation for new Payload variant.

--- src/sync/ms_queue.rs
+++ src/sync/ms_queue.rs
@@ -40,12 +40,6 @@ struct Signal<T> {
     ready: AtomicBool,
 }
 
-impl<T> Node<T> {
-    fn is_data(&self) -> bool {
-        if let Payload::Data(_) = self.payload { true } else { false }
-    }
-}
-
 // Any particular `T` should never accessed concurrently, so no need
 // for Sync.
 unsafe impl<T: Send> Sync for MsQueue<T> {}
@@ -66,6 +60,12 @@ impl<T> MsQueue<T> {
         let sentinel = q.head.store_and_ref(sentinel, Relaxed, &guard);
         q.tail.store_shared(Some(sentinel), Relaxed);
         q
+    }
+
+    #[inline(always)]
+    /// Get the value of the head pointer
+    fn get_head(&self, guard: &epoch::Guard) -> *mut Node<T> {
+        self.head.load(Relaxed, &guard).unwrap().as_raw()
     }
 
     #[inline(always)]
@@ -141,43 +141,41 @@ impl<T> MsQueue<T> {
             let tail = self.tail.load(Acquire, &guard).unwrap();
 
             // Is the queue in Data mode (empty queues can be viewed as either mode)?
-            if tail.is_data() ||
-                self.head.load(Relaxed, &guard).unwrap().as_raw() == tail.as_raw()
-            {
-                // Attempt to push onto the `tail` snapshot; fails if
-                // `tail.next` has changed, which will always be the case if the
-                // queue has transitioned to blocking mode.
-                match self.push_internal(&guard, tail, cache.into_node()) {
-                    Ok(_) => return,
-                    Err(n) => {
-                        // replace the cache, retry whole thing
-                        cache = Cache::Node(n)
-                    }
-                }
-            } else {
-                // Queue is in blocking mode. Attempt to unblock a thread.
-                let head = self.head.load(Acquire, &guard).unwrap();
-                // Get a handle on the first blocked node. Racy, so queue might
-                // be empty or in data mode by the time we see it.
-                let request = head.next.load(Acquire, &guard).and_then(|next| {
-                    match next.payload {
-                        Payload::Blocked(signal) => Some((next, signal)),
-                        Payload::Data(_) => None,
-                    }
-                });
-                if let Some((blocked_node, signal)) = request {
-                    // race to dequeue the node
-                    if self.head.cas_shared(Some(head), Some(blocked_node), Release) {
-                        unsafe {
-                            // signal the thread
-                            (*signal).data = Some(cache.into_data());
-                            (*signal).ready.store(true, Relaxed);
-                            (*signal).thread.unpark();
-                            guard.unlinked(head);
-                            return;
+            match tail.payload {
+                Payload::Blocked(_) if self.get_head(&guard) != tail.as_raw() => {
+                    // Queue is in blocking mode. Attempt to unblock a thread.
+                    let head = self.head.load(Acquire, &guard).unwrap();
+                    // Get a handle on the first blocked node. Racy, so queue might
+                    // be empty or in data mode by the time we see it.
+                    let request = head.next.load(Acquire, &guard).and_then(|next| {
+                        match next.payload {
+                            Payload::Blocked(signal) => Some((next, signal)),
+                            Payload::Data(_) => None,
+                        }
+                    });
+                    if let Some((blocked_node, signal)) = request {
+                        // race to dequeue the node
+                        if self.head.cas_shared(Some(head), Some(blocked_node), Release) {
+                            unsafe {
+                                // signal the thread
+                                (*signal).data = Some(cache.into_data());
+                                (*signal).ready.store(true, Relaxed);
+                                (*signal).thread.unpark();
+                                guard.unlinked(head);
+                                return;
+                            }
                         }
                     }
-                }
+                },
+                _ => {
+                    // Attempt to push onto the `tail` snapshot; fails if
+                    // `tail.next` has changed, which will always be the case if the
+                    // queue has transitioned to blocking mode.
+                    cache = match self.push_internal(&guard, tail, cache.into_node()) {
+                        Ok(_) => return,
+                        Err(n) => Cache::Node(n), // replace the cache, retry whole thing
+                    };
+                },
             }
         }
     }
@@ -277,29 +275,29 @@ impl<T> MsQueue<T> {
             let tail = self.tail.load(Relaxed, &guard).unwrap();
 
             // Double-check that we're in blocking mode
-            if tail.is_data() {
+            match tail.payload {
                 // The current tail is in data mode, so we probably need to abort.
                 // BUT, it might be the sentinel, so check for that first.
-                let head = self.head.load(Relaxed, &guard).unwrap();
-                if tail.is_data() && tail.as_raw() != head.as_raw() { continue; }
-            }
-
-            // At this point, the tail snapshot is either a blocked node deep in
-            // the queue, the sentinel, or no longer accessible from the queue.
-            // In *ALL* of these cases, if we succeed in pushing onto the
-            // snapshot, we know we are maintaining the core invariant: all
-            // reachable, non-sentinel nodes have the same payload mode, in this
-            // case, blocked.
-            match self.push_internal(&guard, tail, node) {
-                Ok(()) => {
-                    while !signal.ready.load(Relaxed) {
-                        thread::park();
+                Payload::Data(_) if self.get_head(&guard) != tail.as_raw() => continue,
+                _ => {
+                    // At this point, the tail snapshot is either a blocked node deep in
+                    // the queue, the sentinel, or no longer accessible from the queue.
+                    // In *ALL* of these cases, if we succeed in pushing onto the
+                    // snapshot, we know we are maintaining the core invariant: all
+                    // reachable, non-sentinel nodes have the same payload mode, in this
+                    // case, blocked.
+                    match self.push_internal(&guard, tail, node) {
+                        Ok(()) => {
+                            while !signal.ready.load(Relaxed) {
+                                thread::park();
+                            }
+                            return signal.data.unwrap();
+                        }
+                        Err(n) => {
+                            node = n;
+                        }
                     }
-                    return signal.data.unwrap();
-                }
-                Err(n) => {
-                    node = n;
-                }
+                },
             }
         }
     }
