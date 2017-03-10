3a5e08cf631cf272d6a3f61472200fe7a6365c75
ticki <ticki@users.noreply.github.com>
2017-03-10 11:44:07 +0100

    Remove `AtomicOption` as it is already covered by `epoch::Atomic`.

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -1,12 +1,9 @@
 //! Synchronization primitives.
 
 pub use self::ms_queue::MsQueue;
-pub use self::atomic_option::AtomicOption;
-pub use self::treiber_stack::TreiberStack;
 pub use self::seg_queue::SegQueue;
-pub use self::arc_cell::ArcCell;
+pub use self::treiber_stack::TreiberStack;
 
-mod atomic_option;
 mod ms_queue;
-mod treiber_stack;
 mod seg_queue;
+mod treiber_stack;
