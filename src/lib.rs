b7f73d13b140cb3041cd1f7cff469fb843cbecaa
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2018-02-11 01:10:32 +0900

    Update the top-level crate doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,29 @@
-//! Support for concurrent and parallel programming.
+//! Support for concurrent programming: memory management, synchronization,
+//! non-blocking data structures.
 //!
-//! This crate is an early work in progress. The focus for the moment is
-//! concurrency:
+//! Crossbeam consists of several subcrates.
 //!
-//! - **Non-blocking data structures**. These data structures allow for high
-//! performance, highly-concurrent access, much superior to wrapping with a
-//! `Mutex`. Ultimately the goal is to include stacks, queues, deques, bags,
-//! sets and maps. These live in the `sync` module.
+//! - `crossbeam-epoch` for **Memory management**. Because non-blocking data
+//!   structures avoid global synchronization, it is not easy to tell when
+//!   internal data can be safely freed. The crate provides generic, easy to
+//!   use, and high-performance APIs for managing memory in these cases. We plan
+//!   to support other memory management schemes, e.g. hazard pointers (HP) and
+//!   quiescent state-based reclamation (QSBR). The crate is reexported as the
+//!   `epoch` module.
 //!
-//! - **Memory management**. Because non-blocking data structures avoid global
-//! synchronization, it is not easy to tell when internal data can be safely
-//! freed. The `mem` module provides generic, easy to use, and high-performance
-//! APIs for managing memory in these cases. These live in the `mem` module.
+//! - `crossbeam-utils` for **Utilities**. A "scoped" thread API makes it
+//!   possible to spawn threads that share stack data with their
+//!   parents. `CachePadded` struct inserts pad to align data with
+//!   cacheline. This crate also seeks to expand the standard library's a few
+//!   synchronization primitives (locks, barriers, etc) to include
+//!   advanced/niche primitives, as well as userspace alternatives. This crate
+//!   is reexported as the `utils` module. `CachePadded` and scoped thread API
+//!   is also reexported at the top-level.
 //!
-//! - **Synchronization**. The standard library provides a few synchronization
-//! primitives (locks, barriers, etc) but this crate seeks to expand that set
-//! to include more advanced/niche primitives, as well as userspace
-//! alternatives. These live in the `sync` module.
-//!
-//! - **Scoped thread API**. Finally, the crate provides a "scoped" thread API,
-//! making it possible to spawn threads that share stack data with their
-//! parents. This functionality is exported at the top-level.
+//! - **Non-blocking data structures**. Several crates provide high performance
+//!   and highly-concurrent data structures, which are much superior to wrapping
+//!   with a `Mutex`. Ultimately the goal is to include stacks, queues, deques,
+//!   bags, sets and maps. These subcrates are reexported in the `sync` module.
 
 //#![deny(missing_docs)]
 
