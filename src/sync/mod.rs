acda356b285757773ce0ef2d05864b21313586eb
Stjepan Glavina <stjepang@gmail.com>
2018-10-25 02:29:19 +0200

    Add WaitGroup synchronization primitive (#50)
    * Add WaitGroup synchronization primitive
    
    * Optimize drop
    
    * Document differences from Barrier

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -1,5 +1,7 @@
 //! Synchronization primitives.
 
 mod sharded_lock;
+mod wait_group;
 
 pub use self::sharded_lock::{ShardedLock, ShardedLockReadGuard, ShardedLockWriteGuard};
+pub use self::wait_group::WaitGroup;
