b7145549629596833007cb1278db32909c125b77
Stjepan Glavina <stjepang@gmail.com>
2018-11-05 01:31:14 +0100

    Update Travis config

--- crossbeam-deque/ci/script.sh
+++ crossbeam-deque/ci/script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-check_min_version() {
+check_min_rustc() {
     local rustc="`rustc -V | cut -d' ' -f2 | cut -d- -f1`"
     if [[ "$rustc" != "`echo -e "$rustc\n$1" | sort -V | tail -n1`" ]]; then
-        echo "Unsupported Rust version: $1 < $rustc"
+        echo "Unsupported Rust version: $rustc < $1"
         exit 0
     fi
 }
-check_min_version 1.25.0
+check_min_rustc 1.25.0
 
 set -ex
 
