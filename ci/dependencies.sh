6b3eccee6165eb6ae7fddc809b5bfaeb537147f2
Stjepan Glavina <stjepang@gmail.com>
2018-11-05 17:06:21 +0100

    Print the dependency tree

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -7,5 +7,9 @@ if [[ ! -x "$(command -v cargo-tree)" ]]; then
     cargo install --debug cargo-tree || exit 1
 fi
 
+cargo tree
+
+cargo tree --duplicate
+
 (cargo tree --duplicate | grep "^crossbeam") && exit 1
 exit 0
