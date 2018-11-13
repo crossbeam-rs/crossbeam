c3adfa8dd4c40acf95c476bd4fa9b287ed089b70
Stjepan Glavina <stjepang@gmail.com>
2018-11-13 12:35:22 +0100

    Update licensing

--- crossbeam-channel/src/flavors/array.rs
+++ crossbeam-channel/src/flavors/array.rs
@@ -2,42 +2,16 @@
 //!
 //! This flavor has a fixed, positive capacity.
 //!
-//! # Copyright
-//!
 //! The implementation is based on Dmitry Vyukov's bounded MPMC queue.
 //!
-//! Author: Dmitry Vyukov
-//! License: http://www.1024cores.net/home/code-license
-//! Sources:
+//! Source:
 //!   - http://www.1024cores.net/home/lock-free-algorithms/queues/bounded-mpmc-queue
 //!   - https://docs.google.com/document/d/1yIAYmbvL3JxOKOjuCyon7JhW4cSv1wy5hC0ApeGMV9s/pub
 //!
-//! ```text
-//! Copyright (c) 2010-2011 Dmitry Vyukov. All rights reserved.
-//!
-//! Redistribution and use in source and binary forms, with or without modification, are permitted
-//! provided that the following conditions are met:
-//!
-//!    1. Redistributions of source code must retain the above copyright notice, this list of
-//!       conditions and the following disclaimer.
-//!
-//!    2. Redistributions in binary form must reproduce the above copyright notice, this list
-//!       of conditions and the following disclaimer in the documentation and/or other materials
-//!       provided with the distribution.
-//!
-//! THIS SOFTWARE IS PROVIDED BY DMITRY VYUKOV "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-//! INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-//! PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DMITRY VYUKOV OR CONTRIBUTORS BE LIABLE
-//! FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-//! OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-//! STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-//! OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//!
-//! The views and conclusions contained in the software and documentation are those of the authors
-//! and should not be interpreted as representing official policies, either expressed or implied,
-//! of Dmitry Vyukov.
-//! ```
+//! Copyright & License:
+//!   - Copyright (c) 2010-2011 Dmitry Vyukov
+//!   - Simplified BSD License and Apache License, Version 2.0
+//!   - http://www.1024cores.net/home/code-license
 
 use std::cell::UnsafeCell;
 use std::marker::PhantomData;
