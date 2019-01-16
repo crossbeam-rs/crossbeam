e7e841cbad7a84af65b92cfdbb76cdaaabc68e94
Stjepan Glavina <stjepang@gmail.com>
2019-01-16 18:14:49 +0100

    Add more tests

--- crossbeam-deque/tests/fifo.rs
+++ crossbeam-deque/tests/fifo.rs
@@ -1,14 +1,15 @@
 extern crate crossbeam_deque as deque;
+extern crate crossbeam_utils as utils;
 extern crate rand;
 
 use std::sync::atomic::Ordering::SeqCst;
 use std::sync::atomic::{AtomicBool, AtomicUsize};
 use std::sync::{Arc, Mutex};
-use std::thread;
 
 use deque::Steal::{Empty, Success};
 use deque::Worker;
 use rand::Rng;
+use utils::thread::scope;
 
 #[test]
 fn smoke() {
@@ -73,26 +74,30 @@ fn is_empty() {
 }
 
 #[test]
-fn steal_push() {
+fn spsc() {
     const STEPS: usize = 50_000;
 
     let w = Worker::new_fifo();
     let s = w.stealer();
-    let t = thread::spawn(move || {
-        for i in 0..STEPS {
-            loop {
-                if let Success(v) = s.steal() {
-                    assert_eq!(i, v);
-                    break;
+
+    scope(|scope| {
+        scope.spawn(|_| {
+            for i in 0..STEPS {
+                loop {
+                    if let Success(v) = s.steal() {
+                        assert_eq!(i, v);
+                        break;
+                    }
                 }
             }
-        }
-    });
 
-    for i in 0..STEPS {
-        w.push(i);
-    }
-    t.join().unwrap();
+            assert_eq!(s.steal(), Empty);
+        });
+
+        for i in 0..STEPS {
+            w.push(i);
+        }
+    }).unwrap();
 }
 
 #[test]
@@ -107,12 +112,12 @@ fn stampede() {
     }
     let remaining = Arc::new(AtomicUsize::new(COUNT));
 
-    let threads = (0..THREADS)
-        .map(|_| {
+    scope(|scope| {
+        for _ in 0..THREADS {
             let s = w.stealer();
             let remaining = remaining.clone();
 
-            thread::spawn(move || {
+            scope.spawn(move |_| {
                 let mut last = 0;
                 while remaining.load(SeqCst) > 0 {
                     if let Success(x) = s.steal() {
@@ -121,21 +126,18 @@ fn stampede() {
                         remaining.fetch_sub(1, SeqCst);
                     }
                 }
-            })
-        }).collect::<Vec<_>>();
-
-    let mut last = 0;
-    while remaining.load(SeqCst) > 0 {
-        if let Some(x) = w.pop() {
-            assert!(last < *x);
-            last = *x;
-            remaining.fetch_sub(1, SeqCst);
+            });
         }
-    }
 
-    for t in threads {
-        t.join().unwrap();
-    }
+        let mut last = 0;
+        while remaining.load(SeqCst) > 0 {
+            if let Some(x) = w.pop() {
+                assert!(last < *x);
+                last = *x;
+                remaining.fetch_sub(1, SeqCst);
+            }
+        }
+    }).unwrap();
 }
 
 #[test]
@@ -147,13 +149,13 @@ fn stress() {
     let done = Arc::new(AtomicBool::new(false));
     let hits = Arc::new(AtomicUsize::new(0));
 
-    let threads = (0..THREADS)
-        .map(|_| {
+    scope(|scope| {
+        for _ in 0..THREADS {
             let s = w.stealer();
             let done = done.clone();
             let hits = hits.clone();
 
-            thread::spawn(move || {
+            scope.spawn(move |_| {
                 let w2 = Worker::new_fifo();
 
                 while !done.load(SeqCst) {
@@ -171,32 +173,29 @@ fn stress() {
                         hits.fetch_add(1, SeqCst);
                     }
                 }
-            })
-        }).collect::<Vec<_>>();
+            });
+        }
 
-    let mut rng = rand::thread_rng();
-    let mut expected = 0;
-    while expected < COUNT {
-        if rng.gen_range(0, 3) == 0 {
+        let mut rng = rand::thread_rng();
+        let mut expected = 0;
+        while expected < COUNT {
+            if rng.gen_range(0, 3) == 0 {
+                while let Some(_) = w.pop() {
+                    hits.fetch_add(1, SeqCst);
+                }
+            } else {
+                w.push(expected);
+                expected += 1;
+            }
+        }
+
+        while hits.load(SeqCst) < COUNT {
             while let Some(_) = w.pop() {
                 hits.fetch_add(1, SeqCst);
             }
-        } else {
-            w.push(expected);
-            expected += 1;
         }
-    }
-
-    while hits.load(SeqCst) < COUNT {
-        while let Some(_) = w.pop() {
-            hits.fetch_add(1, SeqCst);
-        }
-    }
-    done.store(true, SeqCst);
-
-    for t in threads {
-        t.join().unwrap();
-    }
+        done.store(true, SeqCst);
+    }).unwrap();
 }
 
 #[test]
@@ -206,61 +205,55 @@ fn no_starvation() {
 
     let w = Worker::new_fifo();
     let done = Arc::new(AtomicBool::new(false));
+    let mut all_hits = Vec::new();
 
-    let (threads, hits): (Vec<_>, Vec<_>) = (0..THREADS)
-        .map(|_| {
+    scope(|scope| {
+        for _ in 0..THREADS {
             let s = w.stealer();
             let done = done.clone();
             let hits = Arc::new(AtomicUsize::new(0));
+            all_hits.push(hits.clone());
 
-            let t = {
-                let hits = hits.clone();
-                thread::spawn(move || {
-                    let w2 = Worker::new_fifo();
+            scope.spawn(move |_| {
+                let w2 = Worker::new_fifo();
 
-                    while !done.load(SeqCst) {
-                        if let Success(_) = s.steal() {
-                            hits.fetch_add(1, SeqCst);
-                        }
-
-                        let _ = s.steal_batch(&w2);
-
-                        if let Success(_) = s.steal_batch_and_pop(&w2) {
-                            hits.fetch_add(1, SeqCst);
-                        }
-
-                        while let Some(_) = w2.pop() {
-                            hits.fetch_add(1, SeqCst);
-                        }
+                while !done.load(SeqCst) {
+                    if let Success(_) = s.steal() {
+                        hits.fetch_add(1, SeqCst);
                     }
-                })
-            };
 
-            (t, hits)
-        }).unzip();
+                    let _ = s.steal_batch(&w2);
 
-    let mut rng = rand::thread_rng();
-    let mut my_hits = 0;
-    loop {
-        for i in 0..rng.gen_range(0, COUNT) {
-            if rng.gen_range(0, 3) == 0 && my_hits == 0 {
-                while let Some(_) = w.pop() {
-                    my_hits += 1;
+                    if let Success(_) = s.steal_batch_and_pop(&w2) {
+                        hits.fetch_add(1, SeqCst);
+                    }
+
+                    while let Some(_) = w2.pop() {
+                        hits.fetch_add(1, SeqCst);
+                    }
                 }
-            } else {
-                w.push(i);
+            });
+        }
+
+        let mut rng = rand::thread_rng();
+        let mut my_hits = 0;
+        loop {
+            for i in 0..rng.gen_range(0, COUNT) {
+                if rng.gen_range(0, 3) == 0 && my_hits == 0 {
+                    while let Some(_) = w.pop() {
+                        my_hits += 1;
+                    }
+                } else {
+                    w.push(i);
+                }
+            }
+
+            if my_hits > 0 && all_hits.iter().all(|h| h.load(SeqCst) > 0) {
+                break;
             }
         }
-
-        if my_hits > 0 && hits.iter().all(|h| h.load(SeqCst) > 0) {
-            break;
-        }
-    }
-    done.store(true, SeqCst);
-
-    for t in threads {
-        t.join().unwrap();
-    }
+        done.store(true, SeqCst);
+    }).unwrap();
 }
 
 #[test]
@@ -285,12 +278,12 @@ fn destructors() {
         w.push(Elem(i, dropped.clone()));
     }
 
-    let threads = (0..THREADS)
-        .map(|_| {
+    scope(|scope| {
+        for _ in 0..THREADS {
             let remaining = remaining.clone();
             let s = w.stealer();
 
-            thread::spawn(move || {
+            scope.spawn(move |_| {
                 let w2 = Worker::new_fifo();
                 let mut cnt = 0;
 
@@ -312,18 +305,15 @@ fn destructors() {
                         remaining.fetch_sub(1, SeqCst);
                     }
                 }
-            })
-        }).collect::<Vec<_>>();
-
-    for _ in 0..STEPS {
-        if let Some(_) = w.pop() {
-            remaining.fetch_sub(1, SeqCst);
+            });
         }
-    }
 
-    for t in threads {
-        t.join().unwrap();
-    }
+        for _ in 0..STEPS {
+            if let Some(_) = w.pop() {
+                remaining.fetch_sub(1, SeqCst);
+            }
+        }
+    }).unwrap();
 
     let rem = remaining.load(SeqCst);
     assert!(rem > 0);
