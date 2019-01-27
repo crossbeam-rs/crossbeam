83c1c4ea9588cb545cf183ae62d85ce0a34b2bde
Stjepan Glavina <stjepang@gmail.com>
2019-01-27 23:31:11 +0100

    Explain why the number of locks is prime

--- crossbeam-utils/src/atomic/atomic_cell.rs
+++ crossbeam-utils/src/atomic/atomic_cell.rs
@@ -695,7 +695,28 @@ impl Drop for WriteGuard {
 #[inline]
 #[must_use]
 fn lock(addr: usize) -> &'static Lock {
-    // The number of locks is prime.
+    /// The number of locks is a prime number because we want to make sure `addr % LEN` gets
+    /// dispersed across all locks.
+    ///
+    /// Note that addresses are always aligned to some power of 2, depending on type `T` in
+    /// `AtomicCell<T>`. If `LEN` was an even number, then `addr % LEN` would be an even number,
+    /// too, which means only half of the locks would get utilized!
+    ///
+    /// It is also possible for addresses to accidentally get aligned to a number that is not a
+    /// power of 2. Consider this example:
+    ///
+    /// ```
+    /// #[repr(C)]
+    /// struct Foo {
+    ///     a: AtomicCell<u8>,
+    ///     b: u8,
+    ///     c: u8,
+    /// }
+    /// ```
+    ///
+    /// Now, if we have a slice of type `&[Foo]`, it is possible that field `a` in all items gets
+    /// stored at addresses that are multiples of 3. It'd be too bad if `LEN` was divisible by 3.
+    /// In order to protect from such cases, we simply choose a large prime number for `LEN`.
     const LEN: usize = 97;
 
     const L: Lock = Lock {
