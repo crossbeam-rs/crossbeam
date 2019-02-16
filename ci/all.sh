6f156c62d4c8364f1939a7a0b4268b8a8da4b004
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-02-16 16:24:44 +0100

    Set test threads to 1 for macOS on Travis
    In an attempt to fix tests with timeouts.

--- ci/all.sh
+++ ci/all.sh
@@ -7,9 +7,9 @@ export RUSTFLAGS="-D warnings"
 
 cargo check --all --no-default-features
 cargo check --all --bins --examples --tests
-cargo test --all
+cargo test --all -- $@
 
 if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
     cargo check --all --no-default-features --features nightly
-    cargo test --all --features nightly
+    cargo test --all --features nightly -- $@
 fi
