11fbf05968ba702b61b6c61e32fdd4250b2c0aba
Manu Thambi <manu@meshcapital.com>
2019-12-18 09:54:15 -0500

    Use integer atomics for for u8, u16, u32, u64 to implement lockfree AtomicCell for types with those widths.
    Integer atomics are stable starting rust 1.34. Use autocfg to cleanup
    the detection of availability of atomics for both stable and nightly.
    
    Issue #454

--- crossbeam-utils/build.rs
+++ crossbeam-utils/build.rs
@@ -3,6 +3,12 @@ extern crate autocfg;
 fn main() {
     let cfg = autocfg::new();
     if cfg.probe_rustc_version(1, 31) {
-        println!("cargo:rustc-cfg=has_min_const_fn");
+        autocfg::emit("has_min_const_fn");
     }
+
+    cfg.emit_type_cfg("core::sync::atomic::AtomicU8", "has_atomic_u8");
+    cfg.emit_type_cfg("core::sync::atomic::AtomicU16", "has_atomic_u16");
+    cfg.emit_type_cfg("core::sync::atomic::AtomicU32", "has_atomic_u32");
+    cfg.emit_type_cfg("core::sync::atomic::AtomicU64", "has_atomic_u64");
+    cfg.emit_type_cfg("core::sync::atomic::AtomicU128", "has_atomic_u128");
 }
