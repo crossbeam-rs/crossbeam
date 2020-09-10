95cdcf2701b642ba7104b482162074b8f06da448
Bernardo Yusti <bernardo.yusti@student.csulb.edu>
2020-09-10 16:13:26 -0700

    Move #[global_allocator] to outside of submodule

--- crossbeam-channel/tests/golang.rs
+++ crossbeam-channel/tests/golang.rs
@@ -9,9 +9,11 @@
 //!   - https://golang.org/LICENSE
 //!   - https://golang.org/PATENTS
 
+use std::alloc::{GlobalAlloc, Layout, System};
 use std::any::Any;
 use std::cell::Cell;
 use std::collections::HashMap;
+use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
 use std::time::Duration;
@@ -164,6 +166,27 @@ impl<F: FnOnce()> Drop for Defer<F> {
         f();
     }
 }
+
+struct Counter;
+
+static ALLOCATED: AtomicUsize = AtomicUsize::new(0);
+unsafe impl GlobalAlloc for Counter {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let ret = System.alloc(layout);
+        if !ret.is_null() {
+            ALLOCATED.fetch_add(layout.size(), SeqCst);
+        }
+        return ret;
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        System.dealloc(ptr, layout);
+        ALLOCATED.fetch_sub(layout.size(), SeqCst);
+    }
+}
+
+#[global_allocator]
+static A: Counter = Counter;
 
 macro_rules! defer {
     ($body:expr) => {
@@ -667,29 +690,6 @@ mod select {
 // https://github.com/golang/go/blob/master/test/chan/select2.go
 mod select2 {
     use super::*;
-    use std::alloc::{GlobalAlloc, Layout, System};
-    use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
-
-    struct Counter;
-
-    static ALLOCATED: AtomicUsize = AtomicUsize::new(0);
-    unsafe impl GlobalAlloc for Counter {
-        unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-            let ret = System.alloc(layout);
-            if !ret.is_null() {
-                ALLOCATED.fetch_add(layout.size(), SeqCst);
-            }
-            return ret;
-        }
-
-        unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-            System.dealloc(ptr, layout);
-            ALLOCATED.fetch_sub(layout.size(), SeqCst);
-        }
-    }
-
-    #[global_allocator]
-    static A: Counter = Counter;
 
     #[test]
     fn main() {
