2b82b8a188f465cf7d1575ab842f26d608be7dc5
Jon Gjengset <jon@thesquareplanet.com>
2020-12-09 17:52:54 -0800

    Why 2 preemptions?

--- ci/crossbeam-epoch-loom.sh
+++ ci/crossbeam-epoch-loom.sh
@@ -5,4 +5,7 @@ set -ex
 
 export RUSTFLAGS="-D warnings --cfg=loom_crossbeam"
 
+# With MAX_PREEMPTIONS=2 the loom tests (currently) take around 11m.
+# If we were to run with =3, they would take several times that,
+# which is probably too costly for CI.
 env LOOM_MAX_PREEMPTIONS=2 cargo test --test loom --features sanitize --release -- --nocapture
