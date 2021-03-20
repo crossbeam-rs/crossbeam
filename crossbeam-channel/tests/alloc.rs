ff88a9d80412b60d8e4f6a5a5b1865d3472156da
iriri <iri@konnichiwastevenspielbergde.su>
2021-03-20 04:12:38 -0700

    Alloc test

--- crossbeam-channel/tests/alloc.rs
+++ crossbeam-channel/tests/alloc.rs
@@ -5,6 +5,7 @@ use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering;
 
 use crossbeam_channel::unbounded;
+use crossbeam_utils::thread::scope;
 
 struct Counter;
 
@@ -21,7 +22,6 @@ unsafe impl GlobalAlloc for Counter {
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
         System.dealloc(ptr, layout);
-        NUM_ALLOCS.fetch_sub(1, Ordering::SeqCst);
     }
 }
 
@@ -35,8 +35,8 @@ fn allocs() {
     // We expect to see the first (and only) additional allocation after exactly
     // BLOCK_CAP sends and receives.
     let before = NUM_ALLOCS.load(Ordering::SeqCst);
-    for _ in 0..30 {
-        s.send(1).unwrap();
+    for i in 0..30 {
+        s.send(i as i32).unwrap();
         r.recv().unwrap();
     }
     let after = NUM_ALLOCS.load(Ordering::SeqCst);
@@ -49,8 +49,8 @@ fn allocs() {
     // Now we can send up to BLOCK_CAP messages before incurring an allocation.
     for _ in 0..100 {
         let before = NUM_ALLOCS.load(Ordering::SeqCst);
-        for _ in 0..31 {
-            s.send(1).unwrap();
+        for i in 0..31 {
+            s.send(i as i32).unwrap();
         }
         for _ in 0..31 {
             r.recv().unwrap();
@@ -58,4 +58,40 @@ fn allocs() {
         let after = NUM_ALLOCS.load(Ordering::SeqCst);
         assert_eq!(before, after);
     }
+
+    let before = NUM_ALLOCS.load(Ordering::SeqCst);
+    scope(|scope| {
+        scope.spawn(|_| {
+            for i in 0..31 * 1_000_000 {
+                s.send(i as i32).unwrap();
+            }
+        });
+        for _ in 0..31 * 1_000_000 {
+            r.recv().unwrap();
+        }
+    })
+    .unwrap();
+    let after = NUM_ALLOCS.load(Ordering::SeqCst);
+    assert!(after < before + 500_000);
+
+    let threads = num_cpus::get();
+    let before = NUM_ALLOCS.load(Ordering::SeqCst);
+    scope(|scope| {
+        for _ in 0..threads / 2 {
+            scope.spawn(|_| {
+                for i in 0..31 * 100_000 {
+                    s.send(i as i32).unwrap();
+                }
+            });
+        }
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+        for _ in 0..threads / 2 {
+            for _ in 0..31 * 100_000 {
+                r.recv().unwrap();
+            }
+        }
+    })
+    .unwrap();
+    let after = NUM_ALLOCS.load(Ordering::SeqCst);
+    assert!(after < before + ((threads / 4) * 100_000));
 }
