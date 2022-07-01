93148ad1dafbf120ba258551f4fc165f2bebf62f
Ralf Jung <post@ralfj.de>
2022-06-30 22:30:55 -0400

    no strict provenance for us
    This depends on once_cell, which is not strict provenance compatible.

--- ci/miri.sh
+++ ci/miri.sh
@@ -10,13 +10,13 @@ echo
 export RUSTFLAGS="${RUSTFLAGS:-} -Z randomize-layout"
 
 # -Zmiri-ignore-leaks is needed because we use detached threads in tests/docs: https://github.com/rust-lang/miri/issues/1371
-MIRIFLAGS="-Zmiri-strict-provenance -Zmiri-symbolic-alignment-check -Zmiri-disable-isolation -Zmiri-ignore-leaks" \
+MIRIFLAGS="-Zmiri-symbolic-alignment-check -Zmiri-disable-isolation -Zmiri-ignore-leaks" \
     cargo miri test \
     -p crossbeam-queue \
     -p crossbeam-utils 2>&1 | ts -i '%.s  '
 
 # -Zmiri-ignore-leaks is needed because we use detached threads in tests/docs: https://github.com/rust-lang/miri/issues/1371
-MIRIFLAGS="-Zmiri-strict-provenance -Zmiri-symbolic-alignment-check -Zmiri-disable-isolation -Zmiri-ignore-leaks" \
+MIRIFLAGS="-Zmiri-symbolic-alignment-check -Zmiri-disable-isolation -Zmiri-ignore-leaks" \
     cargo miri test \
     -p crossbeam-channel 2>&1 | ts -i '%.s  '
 
