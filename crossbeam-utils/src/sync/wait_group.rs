3b5034429a5585974ed3b01d63196aa4791ef035
Taiki Endo <te316e89@gmail.com>
2022-11-17 12:05:30 +0900

    Fix clippy::explicit_auto_deref warning
    ```
    error: deref which would be done by auto-deref
       --> crossbeam-utils/src/sync/wait_group.rs:142:29
        |
    142 |         let count: &usize = &*self.inner.count.lock().unwrap();
        |                             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: try this: `&self.inner.count.lock().unwrap()`
        |
        = note: `-D clippy::explicit-auto-deref` implied by `-D warnings`
        = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#explicit_auto_deref
    ```

--- crossbeam-utils/src/sync/wait_group.rs
+++ crossbeam-utils/src/sync/wait_group.rs
@@ -139,7 +139,7 @@ impl Clone for WaitGroup {
 
 impl fmt::Debug for WaitGroup {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let count: &usize = &*self.inner.count.lock().unwrap();
+        let count: &usize = &self.inner.count.lock().unwrap();
         f.debug_struct("WaitGroup").field("count", count).finish()
     }
 }
