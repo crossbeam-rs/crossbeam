120a6dd41229b1fffc893d33ed5668e00d7f22d5
Taiki Endo <te316e89@gmail.com>
2022-12-27 21:35:46 +0900

    Run cargo-careful in CI
    https://github.com/RalfJung/cargo-careful

---
+++ ci/careful.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euxo pipefail
+IFS=$'\n\t'
+cd "$(dirname "$0")"/..
+
+export RUSTFLAGS="${RUSTFLAGS:-} -Z randomize-layout"
+
+cargo careful test --all --all-features --exclude benchmarks -- --test-threads=1
