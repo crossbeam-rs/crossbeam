e28e55005fe21901f1ad8af4be4bdd320c1b9153
Dirreke <mingyang_ge@163.com>
2023-07-14 19:57:37 +0800

    add support for csky arch

--- no_atomic.rs
+++ no_atomic.rs
@@ -27,6 +27,7 @@ const NO_ATOMIC_64: &[&str] = &[
     "armv5te-unknown-linux-uclibceabi",
     "armv6k-nintendo-3ds",
     "avr-unknown-gnu-atmega328",
+    "csky-unknown-linux-gnuabiv2",
     "hexagon-unknown-linux-musl",
     "m68k-unknown-linux-gnu",
     "mips-unknown-linux-gnu",
