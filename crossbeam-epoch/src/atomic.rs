f2e8548bbb7ac540a370c35d5e79c1e2155a35b5
Taiki Endo <te316e89@gmail.com>
2023-07-16 14:42:51 +0900

    Fix clippy::incorrect_clone_impl_on_copy_type warning
    ```
    error: incorrect implementation of `clone` on a `Copy` type
        --> crossbeam-epoch/src/atomic.rs:1171:29
         |
    1171 |       fn clone(&self) -> Self {
         |  _____________________________^
    1172 | |         Self {
    1173 | |             data: self.data,
    1174 | |             _marker: PhantomData,
    1175 | |         }
    1176 | |     }
         | |_____^ help: change this to: `{ *self }`
         |
         = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#incorrect_clone_impl_on_copy_type
         = note: `#[deny(clippy::incorrect_clone_impl_on_copy_type)]` on by default
    
    error: incorrect implementation of `clone` on a `Copy` type
      --> crossbeam-deque/src/deque.rs:82:34
       |
    82 |       fn clone(&self) -> Buffer<T> {
       |  __________________________________^
    83 | |         Buffer {
    84 | |             ptr: self.ptr,
    85 | |             cap: self.cap,
    86 | |         }
    87 | |     }
       | |_____^ help: change this to: `{ *self }`
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#incorrect_clone_impl_on_copy_type
       = note: `#[deny(clippy::incorrect_clone_impl_on_copy_type)]` on by default
    ```

--- crossbeam-epoch/src/atomic.rs
+++ crossbeam-epoch/src/atomic.rs
@@ -1169,10 +1169,7 @@ pub struct Shared<'g, T: 'g + ?Sized + Pointable> {
 
 impl<T: ?Sized + Pointable> Clone for Shared<'_, T> {
     fn clone(&self) -> Self {
-        Self {
-            data: self.data,
-            _marker: PhantomData,
-        }
+        *self
     }
 }
 
