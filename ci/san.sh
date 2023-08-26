518c0b9f9acc57063ce2ccacf06d8fd40e596c13
Taiki Endo <te316e89@gmail.com>
2023-08-26 16:41:29 +0900

    ci: Remove needless cargo clean from san.sh

--- ci/san.sh
+++ ci/san.sh
@@ -20,17 +20,14 @@ RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
 
 # There are memory leaks in crossbeam-skiplist.
 # https://github.com/crossbeam-rs/crossbeam/issues/614
-cargo clean
 RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
     cargo test --release --target x86_64-unknown-linux-gnu \
     -p crossbeam-skiplist --test map --test set
-cargo clean
 ASAN_OPTIONS="detect_leaks=0" \
     RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
     cargo test --release --target x86_64-unknown-linux-gnu \
     -p crossbeam-skiplist --tests
 
-cargo clean
 RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
     cargo run \
     --release \
