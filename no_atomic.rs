dcb3acc462ebca44ad03f46c367425f6f8d5f248
Taiki Endo <te316e89@gmail.com>
2023-10-25 01:24:44 +0000

    Update no_atomic.rs

--- no_atomic.rs
+++ no_atomic.rs
@@ -27,6 +27,7 @@ const NO_ATOMIC_64: &[&str] = &[
     "armv5te-unknown-linux-uclibceabi",
     "armv6k-nintendo-3ds",
     "csky-unknown-linux-gnuabiv2",
+    "csky-unknown-linux-gnuabiv2hf",
     "hexagon-unknown-linux-musl",
     "m68k-unknown-linux-gnu",
     "mips-unknown-linux-gnu",
