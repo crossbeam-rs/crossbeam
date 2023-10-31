17fecd9a442ab1582f75c81ce945bc4a66dc5c1e
Taiki Endo <te316e89@gmail.com>
2023-10-31 01:25:12 +0000

    Update no_atomic.rs

--- no_atomic.rs
+++ no_atomic.rs
@@ -37,6 +37,7 @@ const NO_ATOMIC_64: &[&str] = &[
     "mipsel-unknown-linux-gnu",
     "mipsel-unknown-linux-musl",
     "mipsel-unknown-linux-uclibc",
+    "mipsel-unknown-netbsd",
     "mipsel-unknown-none",
     "mipsisa32r6-unknown-linux-gnu",
     "mipsisa32r6el-unknown-linux-gnu",
