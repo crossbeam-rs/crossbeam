4ef4f09a8963f2da3eaa0b1d1cee8b4a67116513
Taiki Endo <te316e89@gmail.com>
2024-01-06 21:49:39 +0900

    Ignore dead_code warnings for tuple structs
    ```
    warning: field `0` is never read
       --> crossbeam-epoch/src/collector.rs:290:21
        |
    290 |         struct Elem(i32);
        |                ---- ^^^
        |                |
        |                field in this struct
        |
        = note: `#[warn(dead_code)]` on by default
    help: consider changing the field to be of unit type to suppress this warning while preserving the field numbering, or remove the field
        |
    290 |         struct Elem(());
        |                     ~~
    
    warning: field `0` is never read
       --> crossbeam-epoch/src/collector.rs:354:21
        |
    354 |         struct Elem(i32);
        |                ---- ^^^
        |                |
        |                field in this struct
        |
    help: consider changing the field to be of unit type to suppress this warning while preserving the field numbering, or remove the field
        |
    354 |         struct Elem(());
        |                     ~~
    
    warning: field `0` is never read
       --> crossbeam-epoch/src/collector.rs:431:21
        |
    431 |         struct Elem(i32);
        |                ---- ^^^
        |                |
        |                field in this struct
        |
    help: consider changing the field to be of unit type to suppress this warning while preserving the field numbering, or remove the field
        |
    431 |         struct Elem(());
        |                     ~~
    
    warning: field `0` is never read
     --> crossbeam-utils/tests/atomic_cell.rs:9:22
      |
    9 |     struct UsizeWrap(usize);
      |            --------- ^^^^^
      |            |
      |            field in this struct
      |
      = note: `#[warn(dead_code)]` on by default
    help: consider changing the field to be of unit type to suppress this warning while preserving the field numbering, or remove the field
      |
    9 |     struct UsizeWrap(());
      |                      ~~
    
    warning: field `0` is never read
      --> crossbeam-utils/tests/atomic_cell.rs:10:19
       |
    10 |     struct U8Wrap(bool);
       |            ------ ^^^^
       |            |
       |            field in this struct
       |
    help: consider changing the field to be of unit type to suppress this warning while preserving the field numbering, or remove the field
       |
    10 |     struct U8Wrap(());
       |                   ~~
    
    warning: field `0` is never read
      --> crossbeam-utils/tests/atomic_cell.rs:11:20
       |
    11 |     struct I16Wrap(i16);
       |            ------- ^^^
       |            |
       |            field in this struct
       |
    help: consider changing the field to be of unit type to suppress this warning while preserving the field numbering, or remove the field
       |
    11 |     struct I16Wrap(());
       |                    ~~
    
    warning: field `0` is never read
      --> crossbeam-utils/tests/atomic_cell.rs:13:22
       |
    13 |     struct U64Align8(u64);
       |            --------- ^^^
       |            |
       |            field in this struct
       |
    help: consider changing the field to be of unit type to suppress this warning while preserving the field numbering, or remove the field
       |
    13 |     struct U64Align8(());
       |                      ~~
    
    warning: field `0` is never read
     --> crossbeam-channel/benchmarks/message.rs:6:27
      |
    6 | pub(crate) struct Message(pub(crate) [usize; LEN]);
      |                   ------- ^^^^^^^^^^^^^^^^^^^^^^^
      |                   |
      |                   field in this struct
      |
      = note: `Message` has a derived impl for the trait `Clone`, but this is intentionally ignored during dead code analysis
      = note: `#[warn(dead_code)]` on by default
    help: consider changing the field to be of unit type to suppress this warning while preserving the field numbering, or remove the field
      |
    6 | pub(crate) struct Message(());
      |                           ~~
    ```

--- crossbeam-channel/benchmarks/message.rs
+++ crossbeam-channel/benchmarks/message.rs
@@ -3,7 +3,7 @@ use std::fmt;
 const LEN: usize = 1;
 
 #[derive(Clone, Copy)]
-pub struct Message(pub [usize; LEN]);
+pub struct Message(#[allow(dead_code)] [usize; LEN]);
 
 impl fmt::Debug for Message {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
