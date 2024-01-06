1711ffa016c9bfae4dd970ee285af94ab8dc012c
Taiki Endo <te316e89@gmail.com>
2024-01-06 21:57:48 +0900

    ci: Remove scripts that call single command

--- ci/clippy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-IFS=$'\n\t'
-cd "$(dirname "$0")"/..
-
-rustup component add clippy
-
-cargo clippy --all --tests --examples
--- ci/docs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-IFS=$'\n\t'
-cd "$(dirname "$0")"/..
-
-cargo doc --no-deps --all --all-features
