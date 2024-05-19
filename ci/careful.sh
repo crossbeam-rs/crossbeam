79c1a6c43dbc5248bbf0289958ff31f8d25a4cf0
Ryo Onodera <ryoqun@gmail.com>
2024-05-19 16:12:33 +0900

    Fix the careful job with a workaround
    ref: https://github.com/RalfJung/cargo-careful/issues/31

--- ci/careful.sh
+++ ci/careful.sh
@@ -3,6 +3,9 @@ set -euxo pipefail
 IFS=$'\n\t'
 cd "$(dirname "$0")"/..
 
-export RUSTFLAGS="${RUSTFLAGS:-} -Z randomize-layout"
+# TODO: Use the system's default linker instead of rust-lld, which recently
+# became the default until this cargo-careful bug is fixed:
+#   https://github.com/RalfJung/cargo-careful/issues/31
+export RUSTFLAGS="${RUSTFLAGS:-} -Z randomize-layout -Z linker-features=-lld"
 
 cargo careful test --all --all-features --exclude benchmarks -- --test-threads=1
