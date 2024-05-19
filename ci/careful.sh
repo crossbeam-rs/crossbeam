8a0b0557738fd04430cc7e22f30cfa021f5d95e5
Ryo Onodera <ryoqun@gmail.com>
2024-05-19 16:22:28 +0900

    Improve TODO wording

--- ci/careful.sh
+++ ci/careful.sh
@@ -3,9 +3,9 @@ set -euxo pipefail
 IFS=$'\n\t'
 cd "$(dirname "$0")"/..
 
-# TODO: Use the system's default linker instead of rust-lld, which recently
-# became the default until this cargo-careful bug is fixed:
-#   https://github.com/RalfJung/cargo-careful/issues/31
+# TODO: Once cargo-careful's bug (https://github.com/RalfJung/cargo-careful/issues/31) is fixed,
+# stop reverting back to the system's default linker, instead of rust-lld, which became the new 
+# default on linux recently (nightly-2024-05-18 and onwards).
 export RUSTFLAGS="${RUSTFLAGS:-} -Z randomize-layout -Z linker-features=-lld"
 
 cargo careful test --all --all-features --exclude benchmarks -- --test-threads=1
