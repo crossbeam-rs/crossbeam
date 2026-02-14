b59f94edf2555b0f2f342ccd0a1ba6e65138beb6
Taiki Endo <te316e89@gmail.com>
2026-02-14 17:59:37 +0900

    epoch: Resolve clippy::non_canonical_partial_ord_impl warning
    ```
    error: non-canonical implementation of `partial_cmp` on an `Ord` type
        --> crossbeam-epoch/src/atomic.rs:1552:1
         |
    1552 | /  impl<'g, T: ?Sized + Pointable> PartialOrd<Shared<'g, T>> for Shared<'g, T> {
    1553 | |      fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
         | | __________________________________________________________________-
    1554 | ||         Some(self.data.cmp(&other.data))
    1555 | ||     }
         | ||_____- help: change this to: `{ Some(self.cmp(other)) }`
    1556 | |  }
         | |__^
         |
         = help: for further information visit https://rust-lang.github.io/rust-clippy/rust-1.93.0/index.html#non_canonical_partial_ord_impl
         = note: `-D clippy::non-canonical-partial-ord-impl` implied by `-D warnings`
         = help: to override `-D warnings` add `#[allow(clippy::non_canonical_partial_ord_impl)]`
    ```

--- crossbeam-epoch/src/atomic.rs
+++ crossbeam-epoch/src/atomic.rs
@@ -1551,7 +1551,7 @@ impl<T: ?Sized + Pointable> Eq for Shared<'_, T> {}
 
 impl<'g, T: ?Sized + Pointable> PartialOrd<Shared<'g, T>> for Shared<'g, T> {
     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-        Some(self.data.cmp(&other.data))
+        Some(self.cmp(other))
     }
 }
 
