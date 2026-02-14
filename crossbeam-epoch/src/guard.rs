02645cd54511a12647431e4962c2b3ac2213c0fa
Jeonghyeon Kim <49356944+powergee@users.noreply.github.com>
2026-02-14 22:08:36 +0900

    epoch: Allow deferred destruction of unsized types (#1201)

--- crossbeam-epoch/src/guard.rs
+++ crossbeam-epoch/src/guard.rs
@@ -1,7 +1,7 @@
 use core::fmt;
 use core::mem;
 
-use crate::atomic::Shared;
+use crate::atomic::{Pointable, Shared};
 use crate::collector::Collector;
 use crate::deferred::Deferred;
 use crate::internal::Local;
@@ -265,7 +265,7 @@ impl Guard {
     /// }
     /// # unsafe { drop(a.into_owned()); } // avoid leak
     /// ```
-    pub unsafe fn defer_destroy<T>(&self, ptr: Shared<'_, T>) {
+    pub unsafe fn defer_destroy<T: ?Sized + Pointable>(&self, ptr: Shared<'_, T>) {
         self.defer_unchecked(move || ptr.into_owned());
     }
 
