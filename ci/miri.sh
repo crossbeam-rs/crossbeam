40839c5d5efc76c7ecd9f2fabd5afbb40f746058
Taiki Endo <te316e89@gmail.com>
2026-02-21 01:30:11 +0900

    ci: Format shell scripts and TOML files, update ShellCheck config

--- ci/miri.sh
+++ ci/miri.sh
@@ -14,31 +14,31 @@ export RUSTDOCFLAGS="${RUSTDOCFLAGS:-} -Z randomize-layout"
 export MIRIFLAGS="${MIRIFLAGS:-} -Zmiri-strict-provenance -Zmiri-symbolic-alignment-check -Zmiri-disable-isolation"
 
 case "${group}" in
-    channel)
-        MIRI_LEAK_CHECK='1' \
-            cargo miri test --all-features \
-            -p crossbeam-channel 2>&1 | ts -i '%.s  '
-        # -Zmiri-ignore-leaks is needed because we use detached threads in tests in tests/golang.rs: https://github.com/rust-lang/miri/issues/1371
-        MIRIFLAGS="${MIRIFLAGS} -Zmiri-ignore-leaks" \
-            cargo miri test --all-features \
-            -p crossbeam-channel --test golang 2>&1 | ts -i '%.s  '
-        ;;
-    others)
-        cargo miri test --all-features \
-            -p crossbeam-queue \
-            -p crossbeam-epoch \
-            -p crossbeam-utils \
-            -p crossbeam-skiplist \
-            -p crossbeam 2>&1 | ts -i '%.s  '
-        # -Zmiri-compare-exchange-weak-failure-rate=0.0 is needed because some sequential tests (e.g.,
-        # doctest of Stealer::steal) incorrectly assume that sequential weak CAS will never fail.
-        # -Zmiri-preemption-rate=0 is needed because this code technically has UB and Miri catches that.
-        MIRIFLAGS="${MIRIFLAGS} -Zmiri-compare-exchange-weak-failure-rate=0.0 -Zmiri-preemption-rate=0" \
-            cargo miri test --all-features \
-            -p crossbeam-deque 2>&1 | ts -i '%.s  '
-        ;;
-    *)
-        echo "unknown crate group '${group}'"
-        exit 1
-        ;;
+  channel)
+    MIRI_LEAK_CHECK='1' \
+      cargo miri test --all-features \
+      -p crossbeam-channel 2>&1 | ts -i '%.s  '
+    # -Zmiri-ignore-leaks is needed because we use detached threads in tests in tests/golang.rs: https://github.com/rust-lang/miri/issues/1371
+    MIRIFLAGS="${MIRIFLAGS} -Zmiri-ignore-leaks" \
+      cargo miri test --all-features \
+      -p crossbeam-channel --test golang 2>&1 | ts -i '%.s  '
+    ;;
+  others)
+    cargo miri test --all-features \
+      -p crossbeam-queue \
+      -p crossbeam-epoch \
+      -p crossbeam-utils \
+      -p crossbeam-skiplist \
+      -p crossbeam 2>&1 | ts -i '%.s  '
+    # -Zmiri-compare-exchange-weak-failure-rate=0.0 is needed because some sequential tests (e.g.,
+    # doctest of Stealer::steal) incorrectly assume that sequential weak CAS will never fail.
+    # -Zmiri-preemption-rate=0 is needed because this code technically has UB and Miri catches that.
+    MIRIFLAGS="${MIRIFLAGS} -Zmiri-compare-exchange-weak-failure-rate=0.0 -Zmiri-preemption-rate=0" \
+      cargo miri test --all-features \
+      -p crossbeam-deque 2>&1 | ts -i '%.s  '
+    ;;
+  *)
+    echo "unknown crate group '${group}'"
+    exit 1
+    ;;
 esac
