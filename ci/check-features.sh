3b48468d4c8172260507f7cb1cf25caf13c40de0
Taiki Endo <te316e89@gmail.com>
2026-02-21 01:30:11 +0900

    ci: Format shell scripts and TOML files, update ShellCheck config

--- ci/check-features.sh
+++ ci/check-features.sh
@@ -7,23 +7,23 @@ cd "$(dirname "$0")"/..
 # * `--no-dev-deps` - build without dev-dependencies to avoid https://github.com/rust-lang/cargo/issues/4866
 # * `--exclude benchmarks` - benchmarks doesn't published.
 if [[ "${RUST_VERSION}" == "msrv" ]]; then
-    cargo hack build --all --feature-powerset --no-dev-deps --exclude crossbeam-utils --exclude benchmarks --rust-version
-    # atomic feature requires Rust 1.74.
-    cargo hack build -p crossbeam-utils --feature-powerset --no-dev-deps --rust-version --exclude-features atomic
-    cargo +1.74 hack build -p crossbeam-utils --feature-powerset --no-dev-deps
+  cargo hack build --all --feature-powerset --no-dev-deps --exclude crossbeam-utils --exclude benchmarks --rust-version
+  # atomic feature requires Rust 1.74.
+  cargo hack build -p crossbeam-utils --feature-powerset --no-dev-deps --rust-version --exclude-features atomic
+  cargo +1.74 hack build -p crossbeam-utils --feature-powerset --no-dev-deps
 else
-    cargo hack build --all --feature-powerset --no-dev-deps --exclude benchmarks
+  cargo hack build --all --feature-powerset --no-dev-deps --exclude benchmarks
 fi
 
 if [[ "${RUST_VERSION}" == "nightly"* ]]; then
-    # Build for no_std environment.
-    # thumbv7m-none-eabi supports atomic CAS.
-    # thumbv6m-none-eabi supports atomic, but not atomic CAS.
-    # riscv32i-unknown-none-elf does not support atomic at all.
-    rustup target add thumbv7m-none-eabi
-    rustup target add thumbv6m-none-eabi
-    rustup target add riscv32i-unknown-none-elf
-    cargo hack build --all --feature-powerset --no-dev-deps --exclude benchmarks --target thumbv7m-none-eabi --skip std,default
-    cargo hack build --all --feature-powerset --no-dev-deps --exclude benchmarks --target thumbv6m-none-eabi --skip std,default
-    cargo hack build --all --feature-powerset --no-dev-deps --exclude benchmarks --target riscv32i-unknown-none-elf --skip std,default
+  # Build for no_std environment.
+  # thumbv7m-none-eabi supports atomic CAS.
+  # thumbv6m-none-eabi supports atomic, but not atomic CAS.
+  # riscv32i-unknown-none-elf does not support atomic at all.
+  rustup target add thumbv7m-none-eabi
+  rustup target add thumbv6m-none-eabi
+  rustup target add riscv32i-unknown-none-elf
+  cargo hack build --all --feature-powerset --no-dev-deps --exclude benchmarks --target thumbv7m-none-eabi --skip std,default
+  cargo hack build --all --feature-powerset --no-dev-deps --exclude benchmarks --target thumbv6m-none-eabi --skip std,default
+  cargo hack build --all --feature-powerset --no-dev-deps --exclude benchmarks --target riscv32i-unknown-none-elf --skip std,default
 fi
