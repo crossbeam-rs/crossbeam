ff0a22b94d26802c5c2968aae37e3596e45d4866
Taiki Endo <te316e89@gmail.com>
2026-02-21 15:43:31 +0900

    Remove needless doc attrs
    Needless since 3cb43fb39936b83a3f842846f10ab5a0a893d22d

--- crossbeam-channel/src/lib.rs
+++ crossbeam-channel/src/lib.rs
@@ -328,10 +328,7 @@
 #![no_std]
 #![doc(test(
     no_crate_inject,
-    attr(
-        deny(warnings, rust_2018_idioms, single_use_lifetimes),
-        allow(dead_code, unused_assignments, unused_variables)
-    )
+    attr(allow(dead_code, unused_assignments, unused_variables))
 ))]
 #![warn(missing_docs, unsafe_op_in_unsafe_fn)]
 
