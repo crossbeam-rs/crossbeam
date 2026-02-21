23b68fb3ee6a0c8afdefb1f45c5675de37e7ee38
Taiki Endo <te316e89@gmail.com>
2026-02-21 17:56:32 +0900

    deque: Use relaxed store after release fence

---
+++ crossbeam-deque/build.rs
@@ -0,0 +1,14 @@
+// The rustc-cfg emitted by the build script are *not* public API.
+
+use std::env;
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rustc-check-cfg=cfg(crossbeam_sanitize_thread)");
+
+    // `cfg(sanitize = "..")` is not stabilized.
+    let sanitize = env::var("CARGO_CFG_SANITIZE").unwrap_or_default();
+    if sanitize.contains("thread") {
+        println!("cargo:rustc-cfg=crossbeam_sanitize_thread");
+    }
+}
