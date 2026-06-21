e242c5c176112114b7ad7e05c5576683fcbbcb63
Nahla <nahla.nee@protonmail.com>
2026-06-20 20:45:28 -0600

    make CachePadded<T> have C repr to allow casting to and from T

--- crossbeam-utils/src/cache_padded.rs
+++ crossbeam-utils/src/cache_padded.rs
@@ -107,19 +107,19 @@ use core::{
         target_arch = "sparc",
         target_arch = "hexagon",
     ),
-    repr(align(32))
+    repr(align(32), C)
 )]
 // m68k has 16-byte cache line size.
 //
 // Sources:
 // - https://github.com/torvalds/linux/blob/3516bd729358a2a9b090c1905bd2a3fa926e24c6/arch/m68k/include/asm/cache.h#L9
-#[cfg_attr(target_arch = "m68k", repr(align(16)))]
+#[cfg_attr(target_arch = "m68k", repr(align(16), C))]
 // s390x has 256-byte cache line size.
 //
 // Sources:
 // - https://github.com/golang/go/blob/3dd58676054223962cd915bb0934d1f9f489d4d2/src/internal/cpu/cpu_s390x.go#L7
 // - https://github.com/torvalds/linux/blob/3516bd729358a2a9b090c1905bd2a3fa926e24c6/arch/s390/include/asm/cache.h#L13
-#[cfg_attr(target_arch = "s390x", repr(align(256)))]
+#[cfg_attr(target_arch = "s390x", repr(align(256), C))]
 // x86, wasm, riscv, and sparc64 have 64-byte cache line size.
 //
 // Sources:
@@ -145,7 +145,7 @@ use core::{
         target_arch = "m68k",
         target_arch = "s390x",
     )),
-    repr(align(64))
+    repr(align(64), C)
 )]
 pub struct CachePadded<T> {
     value: T,
